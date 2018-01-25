96421efaf0ffc7606d7d2b1f6cc28f3ddd2de0a2
walageri <gerygilyia@gmail.com>
2018-01-25 14:46:24 +0700

    firts commit

--- app/src/main/java/com/example/gery/randomcolor/MainActivity.java
+++ app/src/main/java/com/example/gery/randomcolor/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-//Change background
+//Change background random
 public class MainActivity extends AppCompatActivity {
     TextView tv;
     @Override
